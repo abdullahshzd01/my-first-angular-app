e574db4adf3628bb348ecfea571b23a5e142f230
abdullahshzd01 <abdullahshzd.69@gmail.com>
2023-05-03 13:38:09 +0500

    On branch master

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MovieDashboardComponent } from './movie-dashboard/movie-dashboard.component';
+import { MovieTableComponent } from './movie-table/movie-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieDashboardComponent,
+    MovieTableComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/movie-dashboard/movie-dashboard.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-movie-dashboard',
+  templateUrl: './movie-dashboard.component.html',
+  styleUrls: ['./movie-dashboard.component.css']
+})
+export class MovieDashboardComponent {
+//   myFavoriteMovies = [
+//     'Encanto', 
+//     'Spider-Man: No Way Home', 
+//     "Harry Potter and the Sorcerer's Stone"
+//   ];
+
+	myFavoriteMovies = [
+		'Encanto',
+		'Spider-Man: No Way Home',
+		"Harry Potter and the Sorcerer's Stone",
+		'The Shawshank Redemption',
+		'The Dark Knight',
+		'The Godfather'
+	];
+
+	selectedMovieToWatch(data: string) {
+		debugger;
+		alert(data);
+	}
+}
